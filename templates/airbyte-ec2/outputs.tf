ffe9c40b450e9120c2d9098de1d885c77e587aef
Ricardo Ibarra <raib1997@gmail.com>
2022-11-18 11:19:22 -0500

    Change minor details on outputs.tf

--- templates/airbyte-ec2/outputs.tf
+++ templates/airbyte-ec2/outputs.tf
@@ -1,4 +1,4 @@
-# Output for the public DNS of the ASTRONOMER EC2 instance
+# Output for the public DNS of the EC2 instance
 output "airbyte_ec2_dns" {
   value = aws_instance.airbyte_instance.public_dns
 }
